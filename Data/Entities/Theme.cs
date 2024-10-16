23bb39b2cf5454d731c801b929af9128c2a1875d
Juliano Miquelleto <julianomiquelleto@gmail.com>
2024-10-16 14:47:45 -0300

    Estrutura interna de entidades, questoes, capitulos, plan,temas

---
+++ Data/Entities/Theme.cs
@@ -0,0 +1,9 @@
+﻿namespace IAutor.Api.Data.Entities
+{
+    public sealed class Theme:Base
+    {
+        public string Title { get; set; }
+
+        [JsonIgnore] public ICollection<Question>? Questions { get; set; }
+    }
+}
