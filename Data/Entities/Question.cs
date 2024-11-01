623bf50c03725e5d51f5f44023655c2276bfe127
Josué Monteiro <eaglejosue@gmail.com>
2024-11-01 17:17:31 -0300

    fix: token

--- Data/Entities/Question.cs
+++ Data/Entities/Question.cs
@@ -1,12 +1,11 @@
-﻿namespace IAutor.Api.Data.Entities
-{
-    public sealed class Question : Base
-    {
-        public string Title { get; set; }
-        public int MaxLimitCharacters { get; set; } = 10000;
-        public int MinLimitCharacters { get; set; } = 1;
-        public long ChapterId { get; set; }
+﻿namespace IAutor.Api.Data.Entities;
 
-        [JsonIgnore] public Chapter Chapter { get; set; }
-    }
+public sealed class Question : Base
+{
+    public string Title { get; set; }
+    public int MaxLimitCharacters { get; set; } = 10000;
+    public int MinLimitCharacters { get; set; } = 1;
+    public long ChapterId { get; set; }
+
+    [JsonIgnore] public Chapter Chapter { get; set; }
 }
